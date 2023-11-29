ae7620b66b6dfceff8282ddf70140bd0a12c3c44
Davi seabra da Costa <daviseabradacosta@192.168.1.7>
2023-11-29 17:12:33 -0300

    Adicionei um Link que leva para o home nas paginas sobre e trabalhe conosco

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,5 +1,7 @@
 import H1 from "../../components/H1";
-
+import "./index.css"
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 const Sobre = () =>{
 
     return(
@@ -9,6 +11,11 @@ const Sobre = () =>{
 <h3>Bom, nosso restaurante preza pelo meio ambiente
      e estamos com um incrivel projeto de reflorestamento,
       já fizemos o plantil de mais de 100 arvores.</h3>
+
+        <Link to="/" >
+            <Button id={"button"} name="Home" />
+        </Link>
+
         </div>
     )
 
--- src/pages/Trabalhe-conosco/index.js
+++ src/pages/Trabalhe-conosco/index.js
@@ -1,14 +1,17 @@
+import "./index.css"
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 const Contatos = () => {
 
     return (
         <div>
+            <h1>Contatos</h1>
             <h3>Venha fazer parte da nossa equipe culinária apaixonada! No Aromas do mundo, buscamos talentos que compartilhem nosso amor pela gastronomia e dedicação ao serviço excepcional. Se você é apaixonado por criar experiências gastronômicas memoráveis, tem habilidades culinárias excepcionais ou expertise em atendimento ao cliente, Estamos ansiosos para receber indivíduos comprometidos e apaixonados pela arte da culinária</h3>
-            <br></br>
             <h3>Envie seu currículo para aromas.do.mundo@gmail.com.</h3>
-            
             <h3>Telefone para contato: 4002-8922</h3>
-
-
+        <Link to="/">
+            <Button id={"button"} name="Home" />
+        </Link>
         </div>
     );
 }
